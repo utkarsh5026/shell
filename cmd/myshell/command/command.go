bea3bfdd5d2a153773e64dfa16e2ab1c5b9a09bd
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-07-31 19:32:47 +0530

    pass stage

--- cmd/myshell/command/command.go
+++ cmd/myshell/command/command.go
@@ -118,7 +118,7 @@ func CdCommand(commandLine string) error {
 
 	err = os.Chdir(dir)
 	if err != nil {
-		fmt.Println("Error changing directory:", err)
+		fmt.Printf("%s: No such file or directory\n", commandLine)
 		return err
 	}
 	return nil
